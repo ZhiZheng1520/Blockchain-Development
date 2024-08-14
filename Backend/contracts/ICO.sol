1d4694dcf3e03b554bd07f5f4d02c66918959e69
zhern03 <siuhuilow@gmail.com>
2024-08-14 20:27:20 +0800

    added buy token

--- Backend/contracts/ICO.sol
+++ Backend/contracts/ICO.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract ICO is Pausable, Ownable {
+
     IERC20 public token;
     uint public price = 1 ether;
     uint public totalTokensSold;
@@ -18,17 +19,8 @@ contract ICO is Pausable, Ownable {
     function buyToken() external payable whenNotPaused {
         uint256 weiAmount = msg.value;
         uint256 numberOfToken = weiAmount / price * 10 ** 18;
-
         token.transferFrom(owner(), msg.sender, numberOfToken);
         totalTokensSold += numberOfToken;
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
     }
 
     function withdrawal() external onlyOwner {
--- carbonnex/src/pages/api/get-marketplace-listing.js
+++ carbonnex/src/pages/api/get-marketplace-listing.js
@@ -10,4 +10,14 @@ const getUsers = async (req, res) => {
   }
 };
 
+const updateasas = async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM marketplace_list');
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 export default getUsers;
